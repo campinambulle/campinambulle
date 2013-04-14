a5b8a45dd123979c1f00334690ff23e0fc5eb6a0
Jonathan Bonzy <jonathan.bonzy@gmail.com>
2013-04-15 01:03:32 +0200

    Add configuration in contact mail body

--- js/campinambulle.js
+++ js/campinambulle.js
@@ -8,14 +8,20 @@ Campinambulle.carousel = function() {
 };
 
 Campinambulle.computePrice = function() {
-  var totalPrice = 0;
   var configuration = $('input:checked');
+  
+  var totalPrice = 0;
+  var mail = 'mailto:bonjour@campinambulle.com?body=';
   
   for (var i = 0; i < configuration.length; i++) {
     totalPrice += $(configuration[i]).data('price');
+    mail += '- ' + $(configuration[i]).parent().text().trim() + ' ' + $(configuration[i]).parents('.row').find('.prix').text() + '%0d%0a';
   }
   
+  mail += '%0d%0a' + 'Prix : ' + totalPrice + ' € TTC'
+  
   $('.prix-total span').html(totalPrice);
+  $('.contact a').attr('href', mail);
 };
 
 Campinambulle.init = function() {
