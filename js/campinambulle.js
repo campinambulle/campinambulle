e6258d3307359377247d0400571df82105603d2d
Jonathan Bonzy <jonathan.bonzy@gmail.com>
2013-04-15 00:27:08 +0200

    Add base configurator

--- js/campinambulle.js
+++ js/campinambulle.js
@@ -1,8 +1,28 @@
-Campinambulle.init = function() {
+Campinambulle.carousel = function() {
   $('.carousel').carousel();
   
   $('.collapse').collapse({
     hide: true,
     toggle: true
-  })
+  });
+};
+
+Campinambulle.computePrice = function() {
+  var totalPrice = 0;
+  var configuration = $('input:checked');
+  
+  for (var i = 0; i < configuration.length; i++) {
+    totalPrice += $(configuration[i]).data('price');
+  }
+  
+  $('.prix-total span').html(totalPrice);
+};
+
+Campinambulle.init = function() {
+  Campinambulle.carousel();
+  
+  Campinambulle.computePrice();
+  $("input").click(function(){
+    Campinambulle.computePrice();
+  });
 };
