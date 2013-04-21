4b1e4d2073b7d1896e978631d8061b09eae3859a
Jonathan Bonzy <jonathan.bonzy@gmail.com>
2013-04-21 22:22:22 +0200

    Add orange color when a row is checked on config tool

--- js/campinambulle.js
+++ js/campinambulle.js
@@ -24,16 +24,31 @@ Campinambulle.computePrice = function() {
   $('.contact a').attr('href', mail);
 };
 
+Campinambulle.manageCheckboxStatus = function(self) {
+  var checkboxClicked = $(self);
+  var checkboxClickedStatus = $(self).attr('checked');
+  $("input[name='"+checkboxClicked.attr('name')+"']").attr('checked', false);
+  checkboxClicked.attr('checked', checkboxClickedStatus);
+};
+
+Campinambulle.highlightRowChecked = function(self) {
+  var checkboxClicked = $(self);
+  $("input[name='"+checkboxClicked.attr('name')+"']").parents(".row").removeClass("checked");
+  if(checkboxClicked.attr('checked') == "checked") {
+    checkboxClicked.parents(".row").addClass("checked");
+  } else {
+    checkboxClicked.parents(".row").removeClass("checked");
+  }
+};
+
 Campinambulle.init = function() {
   Campinambulle.carousel();
   
   if($("#configurateur").length) {
     Campinambulle.computePrice();
     $("input").click(function(e) {
-      var checkboxClicked = $(this);
-      var checkboxClickedStatus = $(this).attr('checked');
-      $("input[name='"+checkboxClicked.attr('name')+"']").attr('checked', false);
-      checkboxClicked.attr('checked', checkboxClickedStatus);
+      Campinambulle.manageCheckboxStatus(this);
+      Campinambulle.highlightRowChecked(this);
       Campinambulle.computePrice();
     });
   }
