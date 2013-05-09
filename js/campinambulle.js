779e1e6b69116e60548bee4a1757af253c780b93
Jonathan Bonzy <jonathan.bonzy@gmail.com>
2013-05-09 10:33:29 +0200

    Changes on config tool

--- js/campinambulle.js
+++ js/campinambulle.js
@@ -43,8 +43,8 @@ Campinambulle.checkRequiredField = function(fieldId) {
 
 Campinambulle.submitContactForm = function() {
   $('#devis').submit(function() {
-    if(Campinambulle.checkRequiredField('#nom') && Campinambulle.checkRequiredField('#prenom') 
-      && Campinambulle.checkRequiredField('#courriel') && Campinambulle.checkRequiredField('#commentaires')) {
+    if(Campinambulle.checkRequiredField('#nom') && Campinambulle.checkRequiredField('#adresse') 
+      && Campinambulle.checkRequiredField('#courriel') && Campinambulle.checkRequiredField('#amenagement')) {
       $.ajax({
         url: $(this).attr('action'),
         type: $(this).attr('method'),
